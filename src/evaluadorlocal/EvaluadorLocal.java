34545e58e73f85d29ae41edf1bc6d9bfcd691424
Juan Martin Coronato <jmcoronato@gmail.com>
2025-11-16 20:19:12 -0300

    evaluador local

--- src/evaluadorlocal/EvaluadorLocal.java
+++ src/evaluadorlocal/EvaluadorLocal.java
@@ -3,6 +3,8 @@ package evaluadorlocal;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+
 import monticulo.Monticulo;
 
 public class EvaluadorLocal {
@@ -15,9 +17,44 @@ public class EvaluadorLocal {
 	public EvaluadorLocal() {
 
 	}
+	
+	public void evaluadorLocal() {
+		Scanner sc = new Scanner(System.in);
+
+        // 1. Pedir los valores iniciales
+        System.out.println("Ingrese Presupuesto, Nro Fabricantes y Nro Compradores (ej: 1000 4 4)");
+        
+        int P = sc.nextInt();
+        int F = sc.nextInt();
+        int C = sc.nextInt();
+        
+        presupuesto = P;
+
+        // 2. Pedir los datos de los FABRICANTES usando un bucle
+        System.out.println("--- Ingrese las " + F + " condiciones de los fabricantes (Precio CantidadMinima) ---");
+        for (int i = 0; i < F; i++) {
+            System.out.print("Fabricante " + (i + 1) + ": "); // Prompt para cada línea
+            int precio = sc.nextInt();
+            int cant = sc.nextInt();
+            insertarColaPrioridadFabricantes(i + 1, precio, cant);
+        }
+
+        // 3. Pedir los datos de los COMPRADORES usando un bucle
+
+        System.out.println("--- Ingrese las " + C + " condiciones de los compradores (Precio Cantidad) ---");
+        for (int i = 0; i < C; i++) {
+            System.out.print("Comprador " + (i + 1) + ": "); // Prompt para cada línea
+            int precio = sc.nextInt();
+            int cant = sc.nextInt();
+            insertarColaPrioridadCompradores(i + 1, precio, cant);
+        }
+        
+        sc.close();
+	}
 
 	public int ganancia(String path) throws IOException {
-		procesarArchivo(path);
+		//procesarArchivo(path);
+		evaluadorLocal();
 
 		Fabricante fab = (Fabricante) CPfabricantes.remover(), aux;
 		Comprador comp = (Comprador) CPcompradores.verRaiz();
